b70b27fcf8cd8a95b6a882739b042c2c7474d2ac
Oleg <oleg.mukha.2002@gmail.com>
2022-12-20 19:45:17 +0200

    Implemented methods for interacting with table 'cars'

--- src/database/CarServiceMySQL.java
+++ src/database/CarServiceMySQL.java
@@ -1,4 +1,102 @@
 package database;
 
-public class CarServiceMySQL {
+import database.dao.CarDAO;
+import model.Car;
+import model.Parking;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+import static config.Config.*;
+import static queries.QueriesToCarTable.*;
+import static queries.QueriesToParkingTable.FIND_CARS_BY_OWNER;
+
+public class CarServiceMySQL implements CarDAO {
+    private Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://" + DB_HOST + ":" + DB_PORT + "/" + DB_NAME, USER, PASSWORD);
+        } catch (ClassNotFoundException exception) {
+            System.out.println("Driver not loaded!");
+            exception.printStackTrace();
+        } catch (SQLException exception) {
+            System.out.println("Connection not established!");
+            exception.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void close(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+                throw new RuntimeException(exception);
+            }
+        }
+
+    }
+
+    private static void close(Statement statement) {
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+                throw new RuntimeException(exception);
+            }
+        }
+    }
+    @Override
+    public String addCarToCarList(Car car) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = this.getConnection();
+            preparedStatement = connection.prepareStatement(ADD_CAR_TO_CAR_LIST, 5);
+            preparedStatement.setInt(1, car.getCarBrand());
+            preparedStatement.setString(2, car.getCarNumber());
+            preparedStatement.setInt(3, car.getBodyType());
+            preparedStatement.setInt(4, car.getFuelType());
+            preparedStatement.setFloat(5, car.getEngineVolume());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            close(connection);
+            close((Statement) preparedStatement);
+        }
+        return "Car has been successfully added to car list!";
+    }
+
+    @Override
+    public ArrayList<Car> filterCarsByRegion(String regionChars) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ArrayList<Car> searchedCars = new ArrayList<>();
+        try {
+            connection = this.getConnection();
+            preparedStatement = connection.prepareStatement(FILTER_CARS_BY_REGION, 1);
+            preparedStatement.setString(1, regionChars);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Car car = new Car();
+                car.setCarBrand(resultSet.getInt(CAR_BRAND));
+                car.setCarNumber(resultSet.getString(CAR_NUMBER));
+                car.setBodyType(resultSet.getInt(BODY_TYPE));
+                car.setFuelType(resultSet.getInt(FUEL_TYPE));
+                car.setEngineVolume(resultSet.getFloat(ENGINE_VOLUME));
+                searchedCars.add(car);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        } finally {
+            close(connection);
+            close(preparedStatement);
+        }
+        return searchedCars;
+    }
 }
