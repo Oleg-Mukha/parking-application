1e0558fed878a52adb8395d0742d17abb5a745c1
Oleg <oleg.mukha.2002@gmail.com>
2022-12-20 19:47:09 +0200

    Added new methods for interacting with table 'Parking'

--- src/database/dao/ParkingDAO.java
+++ src/database/dao/ParkingDAO.java
@@ -6,7 +6,9 @@ import java.util.ArrayList;
 
 public interface ParkingDAO {
     ArrayList<Parking> showParking();
-    String addCarOnParking();
-    String updateCarOnParking();
-    void removeCarFromParking();
+    ArrayList<Parking> findCarsByOwner(String owner);
+
+    String addCarOnParking(int spot, int carid, int ownerid);
+    String updateCarOnParking(int priorspot, int newspot);
+    String removeCarFromParking(int carid);
 }
