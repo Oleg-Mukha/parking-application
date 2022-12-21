5df29e20e7aa823cdd5dc3c4cfc92cdffbbc4637
Oleg <oleg.mukha.2002@gmail.com>
2022-12-21 03:00:29 +0200

    Added queries to table 'Parking'

--- src/queries/QueriesToCarTable.java
+++ src/queries/QueriesToCarTable.java
@@ -1,7 +1,7 @@
 package queries;
 
 public class QueriesToCarTable {
-    public static final String ADD_CAR_TO_CAR_LIST = "insert into cars(car_brand, car_number, car_body, car_fuel, engine_volume)" +
-            "values(?, ?, ?, ?, ?)";
+    public static final String SELECT_ALL_CARS_FROM_LIST = "select * from cars";
+    public static final String ADD_CAR_TO_CAR_LIST = "select addingCarToCarList(?,?,?,?,?);";
     public static final String FILTER_CARS_BY_REGION = "call selectionCarsByRegion(?);";
 }
